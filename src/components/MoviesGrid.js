68d3a3622a02b4faba7f309676344cc7ca61f71d
Sushan Paudyal <mailtosushan@gmail.com>
2025-07-02 18:01:11 +0545

    Loading movies from JSON

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -5,10 +5,10 @@ export default function MoviesGrid(){
 
     const [movies, setMovies] = useState([]);
 
-
     useEffect(() => {
-        const m = ["a","b","c","d"];
-        setMovies(m);
+        fetch("movies.json")
+            .then(response => response.json())
+            .then(data => setMovies(data));
     }, [])
 
     return(
