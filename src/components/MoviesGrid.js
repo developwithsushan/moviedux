a6498c061d9247d6c8adfc936b17e337982b330c
Sushan Paudyal <mailtosushan@gmail.com>
2025-07-02 17:31:41 +0545

    Creating the movies state with UseState

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../styles.css';
 
 export default function MoviesGrid(){
+
+    const [movies, setMovies] = useState([]);
+
     return(
         <div>
 
