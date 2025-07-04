d4522649183b2d0a3d695de6d18cb48bea0a0eb7
Sushan Paudyal <mailtosushan@gmail.com>
2025-07-04 13:46:49 +0545

    Filter Bar

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -7,6 +7,9 @@ export default function MoviesGrid(){
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
 
+    const [genre, setGenre] = useState("All Genres");
+    const [rating, setRating] = useState("All");
+
     useEffect(() => {
         fetch("movies.json")
             .then(response => response.json())
@@ -27,6 +30,27 @@ export default function MoviesGrid(){
                 type="text" className="search-input" placeholder="Search Movies..."
             value={searchTerm}
             onChange={handleSearchChange}/>
+            <div className="filter-bar">
+                <div className="filter-slot">
+                    <label>Genre</label>
+                    <select className="filter-dropdown">
+                        <option>All Genres</option>
+                        <option>Action</option>
+                        <option>Drama</option>
+                        <option>Fantasy</option>
+                        <option>Horror</option>
+                    </select>
+                </div>
+                <div className="filter-slot">
+                    <label>Rating</label>
+                    <select className="filter-dropdown">
+                    <option>All</option>
+                        <option>Good</option>
+                        <option>Average</option>
+                        <option>Bad</option>
+                    </select>
+                </div>
+            </div>
             <div className="movies-grid">
                 {
                     filteredMovies.map(movie => (
